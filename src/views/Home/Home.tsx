de00536c24c9ab94c70f31079de3f401eb7656fb
zeratul1215 <ztianhong7@gmail.com>
2025-07-10 12:33:22 +0900

    feat: init

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,8 +1,10 @@
 import styles from './home.module.css';
+import { Button } from '@linktivity/link-ui';
 
 const HomeView = () => {
   return (
     <div className={styles.home}>
+      <Button>Click me</Button>
       <h1 className={styles.title}>{'Home'}</h1>
     </div>
   );
