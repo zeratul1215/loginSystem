760c72f49a6475148f6374fb69b343b4ec22fa91
zeratul1215 <ztianhong7@gmail.com>
2025-07-11 12:26:40 +0900

    feat: logout

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,17 +1,23 @@
+import { Button } from '@linktivity/link-ui';
 import styles from './home.module.css';
 // import SignInView from '@/views/SignIn';
 import { useStore } from '@/stores';
+import firebase from '@/constants/firebase';
 
 const HomeView = () => {
   const { auth } = useStore();
 
-  // console.log(auth.user);
+  const handleLogout = () => {
+    firebase.logout();
+    auth.clearUser();
+  };
 
   return (
     <div className={styles.home}>
       <h1>Home</h1>
       <p>User: {auth.user?.email}</p>
       <p>Logged in: {auth.loggedIn ? 'Yes' : 'No'}</p>
+      <Button onClick={handleLogout}>Logout</Button>
     </div>
   );
 };
